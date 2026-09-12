f0226ce283af218ff6f3d8e82c16ed5a757e32d7
va1xakh <vaisakhrs05@gmail.com>
2026-09-13 01:31:53 +0530

    Update MainActivity.kt, SearchEntity.kt, TakeoutParser.kt

--- MainActivity.kt, SearchEntity.kt, TakeoutParser.kt
+++ MainActivity.kt, SearchEntity.kt, TakeoutParser.kt
@@ -6,7 +6,5 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // A clean, blank slate. The app will compile and launch successfully.
-        // We will build the XML UI after the APK is generated.
     }
 }
